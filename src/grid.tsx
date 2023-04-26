752f40d2e44719b54c85b68dded606c8007c0e72
desms11 <121095759+desms11@users.noreply.github.com>
2023-04-27 01:48:13 +0800

    added timer to grid

--- src/grid.tsx
+++ src/grid.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Box from '@mui/system/Box';
 import Grid from '@mui/system/Unstable_Grid';
 import styled from '@mui/system/styled';
-
+import Timer from "./Timer"; // for timer
 import Example from './TS'; // for the list
+
 
 const Item = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,22 +15,18 @@ const Item = styled('div')(({ theme }) => ({
   textAlign: 'center',
 }));
 
+//might need -npm install @mui/material @emotion/react @emotion/styled
 export default function BasicGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid xs={8}>
-            <Example />
+            <Example /> 
         </Grid>
         <Grid xs={4}>
-          <Item>xs=4</Item>
+            <Timer /> 
         </Grid>
-        <Grid xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid xs={8}>
-          <Item>xs=8</Item>
-        </Grid>
+        
       </Grid>
     </Box>
   );
